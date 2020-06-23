c0db2db3befe5d81a8b4f9e992079842855e6f7f
Tiago Pereira <tiagohcp@gmail.com>
2020-06-22 21:35:37 -0300

    Desafio Concluido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,11 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 interface Balance {
   income: number;
@@ -14,15 +21,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const sumIncome = this.transactions
+      .filter(types => types.type === 'income')
+      .map(values => values.value)
+      .reduce(
+        (accumulatedValue, current): number => accumulatedValue + current,
+        0,
+      );
+
+    const sumOutcome = this.transactions
+      .filter(types => types.type === 'outcome')
+      .map(values => values.value)
+      .reduce(
+        (accumulatedValue, current): number => accumulatedValue + current,
+        0,
+      );
+
+    const balance: Balance = {
+      income: sumIncome,
+      outcome: sumOutcome,
+      total: sumIncome - sumOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
